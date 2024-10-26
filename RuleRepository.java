625ed209de0950dcee3aea936561ef999b56860f
SandadiGayathri <sandadigayathri1@gmail.com>
2024-10-26 16:21:28 +0530

    Update RuleRepository.java

--- RuleRepository.java
+++ RuleRepository.java
@@ -1,5 +1,1 @@
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RuleRepository extends JpaRepository<Rule, Long> {
-    // Define repository methods for accessing and storing rules
-}
